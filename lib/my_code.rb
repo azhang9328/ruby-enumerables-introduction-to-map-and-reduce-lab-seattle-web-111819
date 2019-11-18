621c20febf69d90857bbdfdf633704eff0cb15cc
Allen Zhang <azhang9328@gmail.com>
2019-11-18 09:17:14 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,72 @@
 # My Code here....
+def map_to_negativize(array)
+  i = 0 
+  newArray = []
+  while i < array.length do 
+    newArray.push(-(array[i]))
+  i += 1   
+  end   
+  return newArray
+end  
+
+def map_to_no_change(array)
+  i = 0 
+  newArray = []
+  while i < array.length do 
+    newArray.push(array[i])
+  i += 1
+  end
+  return newArray
+end   
+
+def map_to_double(array)
+  i = 0 
+  newArray = []
+  while i < array.length do 
+    newArray.push(array[i] * 2)
+  i += 1
+  end
+  return newArray
+end   
+
+def map_to_square(array)
+  i = 0 
+  newArray = []
+  while i < array.length do 
+    newArray.push(array[i] ** 2)
+  i += 1
+  end
+  return newArray
+end   
+
+def reduce_to_total(array, starting_point=0)
+  i = 0 
+  itotal = 0
+  newArray = []
+  while i < array.length do 
+    itotal = itotal + array[i] 
+  i += 1
+  end
+  newArray.push(itotal + starting_point)
+  return newArray[0]
+end   
+
+def reduce_to_all_true(array)
+  i = 0 
+  truearray = []
+  falsearray = []
+  while i < array.length do 
+    if array[i]
+      truearray.push("True")
+    else   
+    falsearray.push("False")  
+    end  
+  end 
+  puts tf
+  i += 1
+  if falsearray.empty?
+    return false 
+  else 
+    return true 
+  end  
+end  
